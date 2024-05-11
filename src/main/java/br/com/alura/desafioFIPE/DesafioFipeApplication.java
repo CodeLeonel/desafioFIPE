60a33a5dcf0b8c950992e08507bf2ddb0f45bb1e
Hugo Leonel <hugoleonel.code@gmail.com>
2024-05-11 14:31:22 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/alura/desafioFIPE/DesafioFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafioFIPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFipeApplication.class, args);
+	}
+
+}
